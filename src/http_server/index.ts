7471293eaec4c2747cd0251ed5eee1c270ff590b
katsiaryna <eka3973@yandex.ru>
2022-06-24 22:43:59 +0300

    feat: added readStream

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,17 +1,23 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as http from 'http';
+import { createReadStream } from 'fs'
+import { resolve, dirname } from 'path'
+import { createServer } from 'http'
+import { IncomingMessage, ServerResponse } from 'http'
+import { pipeline } from 'stream/promises'
 
-export const httpServer = http.createServer(function (req, res) {
-    const __dirname = path.resolve(path.dirname(''));
-    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
+export const httpServer = createServer(
+    async (req: IncomingMessage, res: ServerResponse) => {
+        try {
+            const __dirname = resolve(dirname(''))
+            const file_path =
+                __dirname +
+                (req.url === '/' ? '/front/index.html' : '/front' + req.url)
+
+            res.writeHead(200)
+
+            await pipeline(createReadStream(file_path), res)
+        } catch (error) {
+            res.writeHead(404)
+            res.end(JSON.stringify(error))
         }
-        res.writeHead(200);
-        res.end(data);
-    });
-});
+    }
+)
